7ec14f958a89a3a4c2510a72ac3635f1d535afcc
Kuzzi <sebastiansiatkowski@gmail.com>
2026-08-18 07:54:53 -0700

    Add Firebase messaging service worker

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/11.3.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.3.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC_BtuwYiwwmDpAJQuRt4x30YyPGTYvZ7s",
+  authDomain: "lyfe-cacf7.firebaseapp.com",
+  projectId: "lyfe-cacf7",
+  storageBucket: "lyfe-cacf7.firebasestorage.app",
+  messagingSenderId: "119442487958",
+  appId: "1:119442487958:web:e218fafb50513ad717e0b7",
+  measurementId: "G-WE8CC23QSC"
+});
+
+firebase.messaging();
